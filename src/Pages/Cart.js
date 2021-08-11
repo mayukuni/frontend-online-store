38b06a0f460b3f288d28483b2b35849e8a734c54
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-08-11 15:49:17 -0300

    requisito 8

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,22 +1,62 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-class Cart extends Component {
+export default class Cart extends Component {
   constructor(props) {
     super(props);
+    this.addToCart = this.addToCart.bind(this);
     this.state = {
+      cartItems: [],
       hasItems: false,
     };
+  }
+
+  componentDidMount() {
+    this.loadCart();
+  }
+
+  addToCart(product) {
+    const { cartItems } = this.state;
+    if (cartItems.some((item) => item.id === product.id)) {
+      cartItems.find((item) => item.id === product.id).quantity += 1;
+      this.setState({ cartItems });
+    } else {
+      this.setState((prevState) => ({
+        cartItems: [...prevState.cartItems, {
+          quantity: 1,
+          id: product.id,
+          product: [product],
+        }],
+      }));
+    }
+    this.cartHandleCounter();
   }
 
   render() {
     const { hasItems } = this.state;
     console.log(hasItems);
     return (
-      <p data-testid="shopping-cart-empty-message">
-        {hasItems ? 'Tem items' : 'Seu carrinho está vazio'}
-      </p>
+      <div className="cart-container">
+        <nav className="cart-nav">
+          <Link className="material-icons previous" to="/"> arrow_back </Link>
+          <div className="cart-nav-title">
+            <span className="material-icons cart cart-icon"> shopping_cart </span>
+            <h2 className="cart-title">Carrinho de Compras</h2>
+          </div>
+        </nav>
+        <div className="cart-main">
+          {((totalCartItems > 0) ? (
+            this.renderCartDetail()
+          ) : (
+            <div className="cart-empty">
+              <span className="material-icons empty-bag"> shopping_bag </span>
+              <h1 className="cart-empty-title" data-testid="shopping-cart-empty-message">
+                Seu carrinho está vazio
+              </h1>
+            </div>
+          ))}
+        </div>
+      </div>
     );
   }
 }
-
-export default Cart;
